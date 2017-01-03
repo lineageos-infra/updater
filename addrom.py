6215237d2621dff610806400581ed53f63ae2645
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-03 13:28:21 -0600

    addrom: add url as an optional parameter

--- addrom.py
+++ addrom.py
@@ -25,12 +25,13 @@ def getargs():
   parser.add_argument('-dt', '--datetime', required=True)
   parser.add_argument('-t', '--romtype', required=True)
   parser.add_argument('-m', '--md5sum', required=True)
+  parser.add_argument('-u', '--url', required=False)
   return parser.parse_args()
 
-def addrom(filename, datetime, device, version, romtype, md5sum):
+def addrom(filename, datetime, device, version, romtype, md5sum, url=None):
   connect('lineage_updater', host=config['dbhost'])
-  Rom(filename=filename, datetime=datetime, device=device, version=version, romtype=romtype, md5sum=md5sum).save()
+  Rom(filename=filename, datetime=datetime, device=device, version=version, romtype=romtype, md5sum=md5sum, url=url).save()
 
 if __name__ == "__main__":
   args = getargs()
-  addrom(args.filename, args.datetime, args.device, args.version, args.romtype, args.md5sum)
+  addrom(args.filename, args.datetime, args.device, args.version, args.romtype, args.md5sum, args.url)
