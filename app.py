7eafc5bfd5df13446d662c10ed177e4a8cec7552
Tom Powell <zifnab@zifnab06.net>
2017-01-07 17:17:29 -0800

    Move addrom into flask CLI
    Change-Id: Ic78a74052cc059bb034c525ce339d86a70a140db

--- app.py
+++ app.py
@@ -15,7 +15,19 @@ app.config.from_pyfile('app.cfg')
 
 db = MongoEngine(app)
 
-@app.route("/api/v1/<string:device>/<string:romtype>")
+@app.cli.command()
+@click.option('--filename', '-f', 'filename', required=True)
+@click.option('--device', '-d', 'device', required=True)
+@click.option('--version', '-v', 'version', required=True)
+@click.option('--datetime,', '-t', 'datetime', required=True)
+@click.option('--romtype', '-r', 'romtype', required=True)
+@click.option('--md5sum', '-m', 'md5sum', required=True)
+@click.option('--url', '-u', 'url', required=True)
+@click.option('--available', '-a', 'available', default=False)
+def addrom(filename, device, version, datetime, romtype, md5sum, url, available):
+    Rom(filename=filename, datetime=datetime, device=device, version=version, romtype=romtype, md5sum=md5sum, url=url, available=available).save()
+
+@app.route('/api/v1/<string:device>/<string:romtype>')
 def index(apiversion, device, romtype):
     r = request.get_json()
     roms = Rom.objects(device=device, romtype=romtype)
@@ -29,15 +41,15 @@ def index(apiversion, device, romtype):
 
     return roms.to_json()
 
-@app.route("/api/v1/requestfile/<string:file_id>")
+@app.route('/api/v1/requestfile/<string:file_id>')
 def requestfile(file_id):
     rom = Rom.objects.get(id=id)
     if not rom['url']:
         url = config['baseurl']
-        if url[-1:] != "/":
-            url += "/"
+        if url[-1:] != '/':
+            url += '/'
         url += rom['filename']
     else:
         url = rom['url']
 
-    return jsonify({ "url": url, "md5sum": rom['md5sum']})
+    return jsonify({ 'url': url, 'md5sum': rom['md5sum']})
