ef98332ee8f700c040d89deb91e4281d78ad495f
Tom Powell <zifnab@zifnab06.net>
2017-01-07 17:13:39 -0800

    pep8 cleanup
    Change-Id: I57423e3edd53871807468f2fa4a5af08a123bdeb

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from database import Rom
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_mongoengine import MongoEngine
 
+import click
 import datetime
 import json
 import os
@@ -14,32 +15,29 @@ app.config.from_pyfile('app.cfg')
 
 db = MongoEngine(app)
 
-@app.route("/api/<string:apiversion>/<string:device>/<string:romtype>")
+@app.route("/api/v1/<string:device>/<string:romtype>")
 def index(apiversion, device, romtype):
-  if apiversion == "v1":
     r = request.get_json()
     roms = Rom.objects(device=device, romtype=romtype)
 
     if r:
-      if 'ro.build.date.utc' in r and r['ro.build.date.utc'].isdigit():
-          devicedate = datetime.datetime.utcfromtimestamp(int(r['ro.build.date.utc']))
-          roms = roms(datetime__gt=devicedate)
-      if 'romversion' in r:
-        roms = roms(version=r['romversion'])
+        if 'ro.build.date.utc' in r and r['ro.build.date.utc'].isdigit():
+            devicedate = datetime.datetime.utcfromtimestamp(int(r['ro.build.date.utc']))
+            roms = roms(datetime__gt=devicedate)
+        if 'romversion' in r:
+            roms = roms(version=r['romversion'])
 
     return roms.to_json()
 
-@app.route("/api/<string:apiversion>/requestfile/<string:id>")
-def requestfile(apiversion=None, id=None):
-  if id:
-    if apiversion == "v1":
-      rom = Rom.objects.get(id=id)
-      if not rom['url']:
+@app.route("/api/v1/requestfile/<string:file_id>")
+def requestfile(file_id):
+    rom = Rom.objects.get(id=id)
+    if not rom['url']:
         url = config['baseurl']
-        if url[-1:] != "/": url += "/"
+        if url[-1:] != "/":
+            url += "/"
         url += rom['filename']
-      else:
+    else:
         url = rom['url']
 
-      return jsonify({ "url": url, "md5sum": rom['md5sum']})
-
+    return jsonify({ "url": url, "md5sum": rom['md5sum']})
--- database.py
+++ database.py
@@ -1,12 +1,11 @@
-#!/usr/bin/python3
-
-from mongoengine import *
+from mongoengine import Document, BooleanField, DateTimeField, StringField
 
 class Rom(Document):
-  filename = StringField(required=True)
-  datetime = DateTimeField(required=True)
-  device = StringField(required=True)
-  version = StringField(required=True)
-  romtype = StringField(required=True)
-  md5sum = StringField(required=True)
-  url = StringField()
+    filename = StringField(required=True)
+    datetime = DateTimeField(required=True)
+    device = StringField(required=True)
+    version = StringField(required=True)
+    romtype = StringField(required=True)
+    md5sum = StringField(required=True)
+    available = BooleanField(required=False, default=False)
+    url = StringField()
