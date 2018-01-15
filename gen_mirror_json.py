10b4ad24ef0356fbc49f0b64868f60205bff7b92
jrior001 <jriordan001@gmail.com>
2018-01-14 20:38:10 -0500

    updater: stop generating unused sha1 in local json generator

--- gen_mirror_json.py
+++ gen_mirror_json.py
@@ -17,13 +17,10 @@ for f in [os.path.join(dp, f) for dp, dn, fn in os.walk(FILE_BASE) for f in fn]:
     filename = f.split('/')[-1]
     # lineage-14.1-20171129-nightly-hiaeul-signed.zip
     _, version, builddate, buildtype, device = os.path.splitext(filename)[0].split('-')
-    print('hashing sha1 for {}'.format(filename), file=sys.stderr)
-    sha1 = hashlib.sha1(data).hexdigest()
     print('hashing sha256 for {}'.format(filename), file=sys.stderr)
     sha256 = hashlib.sha256(data).hexdigest()
     builds.setdefault(device, []).append({
         'sha256': hashlib.sha256(data).hexdigest(),
-        'sha1': hashlib.sha1(data).hexdigest(),
         'size': os.path.getsize(f),
         'date': '{}-{}-{}'.format(builddate[0:4], builddate[4:6], builddate[6:8]),
         'filename': filename,
