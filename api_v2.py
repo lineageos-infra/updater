71546bb6989c29783c268b30721a757ebd55de86
LuK1337 <priv.luk@gmail.com>
2023-02-25 11:10:19 +0100

    Add `query_string=True` for changelog cache
    Should hopefully fix pagination in production.

--- api_v2.py
+++ api_v2.py
@@ -70,7 +70,7 @@ def api_v2_device_builds(device):
 
 @api.route('/changes')
 @extensions.cache.cached()
-def api_v2_changes():
+def api_v2_changes(query_string=True):
     page = request.args.get('page', default=0, type=int)
     changes = get_paginated_changes(gerrit, page=page)
     response = []
