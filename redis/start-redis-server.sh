8930b5e127da0d319336aa5fd0a897d30398430a
LuK1337 <priv.luk@gmail.com>
2025-04-26 17:11:29 +0200

    Get rid of permament redis storage
    With cache timeout set to 1h it just doesn't make any sense to bother
    doing that.

--- redis/start-redis-server.sh
+++ redis/start-redis-server.sh
@@ -1,3 +1,3 @@
 sysctl vm.overcommit_memory=1
 sysctl net.core.somaxconn=1024
-redis-server --requirepass $CACHE_REDIS_PASSWORD --dir /data/
+redis-server --requirepass $CACHE_REDIS_PASSWORD --dir $(mktemp -d)
